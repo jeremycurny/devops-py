45e896a53858f458699e73e5c5198ae5c11e34bc
jeremycurny <jeremy.curny.pro@gmail.com>
2019-05-14 16:58:30 -0400

    Update composer-validate.py

--- src/php/composer-validate.py
+++ src/php/composer-validate.py
@@ -3,15 +3,17 @@
 import argparse, json, os, re, sys
 
 parser = argparse.ArgumentParser(description='Devops Py Composer Validation.')
+parser.add_argument('--minor-syntax-forced-vendors', default=None, help='Vendors forced to have a minor update syntax package version (^)')
 parser.add_argument('argv', metavar='argv', nargs='+', help='Composer file')
 args = parser.parse_args()
 
-if len(args.argv) != 1 :
-  # Argv is missing
-  print "Usage: python " + __file__ + " ./composer.json"
-  sys.exit(1)
-
 composerJsonPath = args.argv[0];
+print args.minor_syntax_forced_vendors
+
+if args.minor_syntax_forced_vendors != None:
+  minorSyntaxForcedVendors = args.minor_syntax_forced_vendors.split(',');
+else:
+  minorSyntaxForcedVendors = [];
 
 if not os.path.exists(composerJsonPath):
   # File does not exists
