26220e92b5d3db713e337db3a159a22db1a40ed6
jeremycurny <jeremy.curny.pro@gmail.com>
2019-05-15 11:18:04 -0400

    Update composer-validate.py

--- src/php/composer-validate.py
+++ src/php/composer-validate.py
@@ -3,17 +3,20 @@
 import argparse, json, os, re, sys
 
 parser = argparse.ArgumentParser(description='Devops Py Composer Validation.')
-parser.add_argument('--minor-syntax-forced-vendors', default=None, help='Vendors forced to have a minor update syntax package version (^)')
+parser.add_argument('--patch-forced-vendors', default=[], help='Vendors forced to have a patch update package version (~)')
+parser.add_argument('--minor-forced-vendors', default=[], help='Vendors forced to have a minor update package version (^)')
 parser.add_argument('argv', metavar='argv', nargs='+', help='Composer file')
 args = parser.parse_args()
 
-composerJsonPath = args.argv[0];
-print args.minor_syntax_forced_vendors
+composerJsonPath = args.argv[0]
+minorForcedVendors = args.minor_forced_vendors
+patchForcedVendors = args.patch_forced_vendors
 
-if args.minor_syntax_forced_vendors != None:
-  minorSyntaxForcedVendors = args.minor_syntax_forced_vendors.split(',');
-else:
-  minorSyntaxForcedVendors = [];
+if minorForcedVendors != []:
+  minorForcedVendors = minorForcedVendors.split(',')
+
+if patchForcedVendors != []:
+  patchForcedVendors = patchForcedVendors.split(',')
 
 if not os.path.exists(composerJsonPath):
   # File does not exists
@@ -28,6 +31,13 @@ composerData = json.loads(composerJsonContent)
 composerRequireData = composerData.get('require', [])
 
 for package,version in composerRequireData.iteritems():
+  vendor=package.split('/')[0]
   if not re.search('^(~|\^)?[0-9]+\.[0-9]+\.[0-9]+', version):
     print "Incorrect version for " + package + ": " + version
+    sys.exit(1)
+  if vendor in minorForcedVendors and not re.search('^\^', version):
+    print "Minor update syntax should be used for vendor: " + vendor + " (" + package + ": " + version + ")"
+    sys.exit(1)
+  if vendor in patchForcedVendors and not re.search('^~', version):
+    print "Patch update syntax should be used for vendor: " + vendor + " (" + package + ": " + version + ")"
     sys.exit(1)
